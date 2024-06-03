17bb2c051e21a2d0fa84f8cde388de8cfaae78f2
Aleksey Sakevich <skvch.al@gmail.com>
2024-06-03 16:41:55 +0500

    Add Cutscene

---
+++ StickmanRun/Assets/Cutscene/Cutscene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    public void Start()
+    {
+        StartCoroutine(WaitForLevel());
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("Start Menu");
+    }
+
+    private IEnumerator WaitForLevel()
+    {
+       
+        yield return new WaitForSeconds(65);
+        SceneManager.LoadScene("Start Menu");
+    }
+}
