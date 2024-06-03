324a23d5765eab4b47c399a00df6e8655aab2cf3
CtREL0K <Kzuev905@gmail.com>
2024-06-03 11:24:14 +0500

    hp

--- StickmanRun/Assets/Stikman/Health.cs
+++ StickmanRun/Assets/Stikman/Health.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Helth : MonoBehaviour, IDamageable
 {
+    [SerializeField] private Image HealthBar;
+    [SerializeField] private Image WhiteHealthBar;
     [SerializeField] private int hp = 100;
     [SerializeField] private Camera playerCamera;
     
     public void TakeDamage(int damage)
     {
         hp -= damage;
+        HealthBar.fillAmount = hp * 0.01f;
         if (hp < 0)
         {
+            WhiteHealthBar.fillAmount = 0.0f;
             gameObject.GetComponent<RandKeyboard>().ActivateDeathMenu();
             playerCamera.transform.parent = null;
             gameObject.SetActive(false);
